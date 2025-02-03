0b1b38263a78e2cc8943957c1c246adb369914a5
Juhee Dhone <juheedhone12@gmail.com>
2025-02-04 01:49:26 +0530

    Build Search input.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,18 +1,24 @@
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 
 const SearchInput = () => {
   return (
-    <>
-      <div className="flex text-sm font-normal border border-black rounded-xl ">
-        <Input
+    <div className=" ">
+      <div className="flex border rounded-lg shadow-sm p-2 text-[10px] font-normal">
+        <input
           type="email"
           id="email"
           placeholder="Subscribe to tips over email"
+          className="outline-none w-0  flex-grow"
         />
-        <Button>Yes, Please!</Button>
+        <Button className="text-[10px]">Yes, Please!</Button>
       </div>
-    </>
+      <div className="flex justify-center space-x-6 pt-2">
+        <img src="/youtube.svg" alt="logo" />
+        <img src="/Vector.svg" alt="logo" />
+        <img src="/twitter.svg" alt="logo" />
+        <img src="/link.svg" alt="logo" />
+      </div>
+    </div>
   );
 };
 
