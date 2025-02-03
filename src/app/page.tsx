2a3e55ed245a21205889e092a4478969fd17c614
Juhee Dhone <juheedhone12@gmail.com>
2025-02-03 23:37:16 +0530

    NavBar Fixed Position.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,34 @@ import SearchInput from "@/components/SearchInput";
 
 const Page = () => {
   return (
-    <>
-      <div>
-        {/* navbar */}
-        <div className="pb-8 max-w-full">
-          <NavBar />
-        </div>
-        <p className="font-bold text-xl pb-4">Tags</p>
-        {/* badges */}
-        <div className="pb-8 gap-2 flex flex-wrap">
-          <Badge name="Fashion" color="#FFC7C7" />
-          <Badge name="photography" color="#C7FFFB" />
-          <Badge name="SEO" color="#C7E4FF" />
-          <Badge name="Content" color="#DEC7FF" />
-          <Badge name="Culture" color="#ffC7e8" />
-        </div>
-        {/* Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <Article heading="Trending marketing hack for fashion" time={2} />
-          <Article heading="Trending marketing hack for fashion" time={2} />
-          <Article heading="Trending marketing hack for fashion" time={2} />
-          <Article heading="Trending marketing hack for fashion" time={2} />
-          <Article heading="Trending marketing hack for fashion" time={2} />
-        </div>
-        {/* search Input */}
-        <div className="pt-4">
-          <SearchInput />
-        </div>
-        {/* <DialogBadge /> */}
+    <div className="pt-16">
+      {/* navbar */}
+
+      <NavBar />
+
+      <p className="font-bold text-xl pb-4 ">Tags</p>
+      {/* badges */}
+      <div className="pb-8 gap-2 flex flex-wrap">
+        <Badge name="Fashion" color="#FFC7C7" />
+        <Badge name="photography" color="#C7FFFB" />
+        <Badge name="SEO" color="#C7E4FF" />
+        <Badge name="Content" color="#DEC7FF" />
+        <Badge name="Culture" color="#ffC7e8" />
       </div>
-    </>
+      {/* Cards */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <Article heading="Trending marketing hack for fashion" time={2} />
+        <Article heading="Trending marketing hack for fashion" time={2} />
+        <Article heading="Trending marketing hack for fashion" time={2} />
+        <Article heading="Trending marketing hack for fashion" time={2} />
+        <Article heading="Trending marketing hack for fashion" time={2} />
+      </div>
+      {/* search Input */}
+      <div className="pt-4">
+        <SearchInput />
+      </div>
+      {/* <DialogBadge /> */}
+    </div>
   );
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,15 @@
 const NavBar = () => {
   return (
-    <>
-      <div className="flex justify-between max-w-full">
-        <div className="flex items-center gap-2">
-          <img src="navLogo.png" alt="logo" className="w-10 h-10" />
-          <h1 className="font-bold text-xl">Marketing Prasad</h1>
-        </div>
-
-        <div>
-          <img src="chamburger.png" alt="filter" />
-        </div>
+    <div className="flex justify-between fixed top-0 right-0 left-0 p-4 bg-white z-10 border-b-2">
+      <div className="flex items-center gap-2">
+        <img src="navLogo.png" alt="logo" className="w-10 h-10" />
+        <h1 className="font-bold text-xl">Marketing Prasad</h1>
       </div>
-    </>
+
+      <div>
+        <img src="chamburger.png" alt="filter" />
+      </div>
+    </div>
   );
 };
 
