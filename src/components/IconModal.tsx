3f0da0928a0454a6152b07fe40df737e25c68191
Juhee Dhone <juheedhone12@gmail.com>
2025-02-05 15:57:44 +0530

    Add icon modal in NavBar.

---
+++ src/components/IconModal.tsx
@@ -0,0 +1,12 @@
+const IconModal = () => {
+	return (
+		<div className="flex justify-center items-center space-x-6 md:space-x-4 pt-2 ">
+			<img src="/youtube.svg" alt="logo" />
+			<img src="/link.svg" alt="logo" />
+			<img src="/Vector.svg" alt="logo" />
+			<img src="/twitter.svg" alt="logo" />
+		</div>
+	);
+};
+
+export default IconModal;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,9 @@
+import IconModal from "./IconModal";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
 	return (
-		<div className="flex justify-between fixed top-0 right-0 left-0 p-4 bg-white z-10 border-b-2">
+		<div className="flex justify-between items-center fixed top-0 right-0 left-0 p-4 bg-white z-10">
 			<div className="flex items-center gap-2 sm:gap-4">
 				<img src="navLogo.png" alt="logo" className="w-10 h-10" />
 				<h1 className="font-bold text-xl max-w-28">Marketing Prasad</h1>
@@ -10,8 +11,11 @@ const NavBar = () => {
 			<div className="hidden md:block">
 				<SearchInput />
 			</div>
-			<div>
+			<div className="md:hidden">
 				<img src="chamburger.png" alt="filter" />
+			</div>
+			<div className="hidden md:block  ">
+				<IconModal />
 			</div>
 		</div>
 	);
