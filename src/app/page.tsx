d1693d89fa5c092b74c3cfc5ad9c6100519aee4c
Shrikant Kalar <shrikantkalar023@gmail.com>
2025-02-05 15:25:09 +0530

    task: tags array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,16 @@ import Article from "@/components/Article";
 import ArticleDialog from "@/components/ArticleDialog";
 import Badge from "@/components/Badge";
 import ShowMoreButton from "@/components/ShowMoreButton";
+import { TAGS } from "@/constants/tags.constants";
 
 const Page = () => {
 	return (
 		<div className=" px-4 pb-36 pt-28 h-full">
 			<p className="font-bold text-xl pb-4 ">Tags</p>
 			<div className="pb-8 gap-2 flex flex-wrap">
-				<Badge name="Fashion" color="#FFC7C7" />
-				<Badge name="Photography" color="#C7FFFB" />
-				<Badge name="SEO" color="#C7E4FF" />
-				<Badge name="Content" color="#DEC7FF" />
-				<Badge name="Culture" color="#ffC7e8" />
+				{TAGS.map((tag) => (
+					<Badge key={tag.name} name={tag.name} color={tag.color} />
+				))}
 				<ShowMoreButton />
 			</div>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
---
+++ src/constants/tags.constants.ts
@@ -0,0 +1,50 @@
+export const TAGS: ITags[] = [
+	{
+		name: "Fashion",
+		color: "#FFC7C7",
+	},
+	{
+		name: "Photography",
+		color: "#C7FFFB",
+	},
+	{
+		name: "SEO",
+		color: "#C7E4FF",
+	},
+	{
+		name: "Content",
+		color: "#DEC7FF",
+	},
+	{
+		name: "Culture",
+		color: "#FFD7FF",
+	},
+	{
+		name: "Business",
+		color: "#FFD7C7",
+	},
+	{
+		name: "Lifestyle",
+		color: "#FFD7E4",
+	},
+	{
+		name: "Technology",
+		color: "#FFD7FF",
+	},
+	{
+		name: "Health",
+		color: "#FFD7C7",
+	},
+	{
+		name: "Travel",
+		color: "#FFD7E4",
+	},
+	{
+		name: "Instagram",
+		color: "#FFD7FF",
+	},
+];
+export interface ITags {
+	name: string;
+	color: string;
+}
