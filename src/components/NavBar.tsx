385a7f631cf6c98b835172a41584dcda3ea845ae
Shrikant Kalar <shrikantkalar023@gmail.com>
2025-02-05 15:16:21 +0530

    task: hide footer in md

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,16 @@
 const NavBar = () => {
-  return (
-    <div className="flex justify-between fixed top-0 right-0 left-0 p-4 bg-white z-10 border-b-2">
-      <div className="flex items-center gap-2">
-        <img src="navLogo.png" alt="logo" className="w-10 h-10" />
-        <h1 className="font-bold text-xl">Marketing Prasad</h1>
-      </div>
+	return (
+		<div className="flex justify-between fixed top-0 right-0 left-0 p-4 bg-white z-10 border-b-2">
+			<div className="flex items-center gap-2 sm:gap-4">
+				<img src="navLogo.png" alt="logo" className="w-10 h-10" />
+				<h1 className="font-bold text-xl max-w-28">Marketing Prasad</h1>
+			</div>
 
-      <div>
-        <img src="chamburger.png" alt="filter" />
-      </div>
-    </div>
-  );
+			<div>
+				<img src="chamburger.png" alt="filter" />
+			</div>
+		</div>
+	);
 };
 
 export default NavBar;
