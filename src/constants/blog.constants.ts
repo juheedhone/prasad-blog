d73d8ac006a37bd90d2f268c9ecbf9b9fe696ddb
Juhee Dhone <juheedhone12@gmail.com>
2025-02-07 21:17:59 +0530

    tg

--- src/constants/blog.constants.ts
+++ src/constants/blog.constants.ts
@@ -70,31 +70,19 @@ export const BLOGS: IBlog[] = [
 		time: 2,
 		tag: "Photography",
 	},
-	{
-		heading: "SEO in 2025: What You Need to Know",
-		time: 2,
-		tag: "SEO",
-	},
+	
 	{
 		heading: "How to Write Engaging Content for Your Blog",
 		time: 2,
 		tag: "Content",
 	},
-	{
-		heading: "Cultural Shifts in the Digital Age",
-		time: 2,
-		tag: "Culture",
-	},
+
 	{
 		heading: "Building a Profitable Online Business",
 		time: 2,
 		tag: "Business",
 	},
-	{
-		heading: "The Ultimate Guide to a Healthy Lifestyle",
-		time: 2,
-		tag: "Lifestyle",
-	},
+	
 	{
 		heading: "Emerging Tech Trends That Will Shape the Future",
 		time: 2,
@@ -110,9 +98,5 @@ export const BLOGS: IBlog[] = [
 		time: 2,
 		tag: "Travel",
 	},
-	{
-		heading: "How to Grow Your Instagram Audience Organically",
-		time: 2,
-		tag: "Instagram",
-	},
+	
 ];
