75babda38d452f9f053076ab651f6108399a78ee
Juhee Dhone <juheedhone12@gmail.com>
2025-02-07 15:46:36 +0530

    increased dummy article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const Page = () => {
 						/>
 					</div>
 					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  xl:grid-cols-4 2xl:grid-cols-5 gap-4 sm:items-center">
-						{Array(10)
+						{Array(20)
 							.fill(0)
 							.map((_, index) => (
 								<ArticleDialog key={index}>
