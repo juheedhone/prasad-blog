42e4dda321cbc91c8116b9786d4e73f82005a53f
juheedhone <juheedhone12@gmail.com>
2025-02-16 18:51:14 +0530

    added tag schema

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,6 +1,5 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from "./blockContent";
+import post from "./post";
+import tag from "./tag";
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, tag, blockContent];
