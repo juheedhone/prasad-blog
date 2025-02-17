b0446cf4b35dd602cd9e1a0cc15a64f00dcdcec4
juheedhone <juheedhone12@gmail.com>
2025-02-18 01:02:18 +0530

    task: build img url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import BlogGrid from "@/components/BlogGrid";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { defineQuery } from "next-sanity";
 import { sanityFetch } from "../../studio/live";
 
@@ -11,7 +13,7 @@ const BLOGS_QUERY = defineQuery(`
     content,
     timeToRead,
     tag->{
-	title, 
+	title,
 	backgroundColor
 	} // Expanding the reference
   } | order(publishedAt desc)
@@ -20,6 +22,13 @@ const BLOGS_QUERY = defineQuery(`
 const Page = async () => {
 	const { data: blogs } = await sanityFetch({ query: BLOGS_QUERY });
 	console.log(blogs);
+	const urlFor = (source: SanityImageSource) =>
+		imageUrlBuilder({ dataset: "production", projectId: "4elu8cpw" }).image(
+			source,
+		);
+
+	const eventImageUrl = blogs ? urlFor(blogs[0].bgImage).url() : null;
+	console.log(eventImageUrl);
 
 	return <BlogGrid />;
 };
