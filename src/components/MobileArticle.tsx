d38e42643226fed537d9b5b3ae371521f23a7c3e
juheedhone <juheedhone12@gmail.com>
2025-02-23 00:53:14 +0530

    task: image render in portable text

--- src/components/MobileArticle.tsx
+++ src/components/MobileArticle.tsx
@@ -1,78 +1,79 @@
-"use client";
-
-import type { IBlogWithContent } from "@/constants/fetch";
-import { Cross2Icon, EyeOpenIcon, ReloadIcon } from "@radix-ui/react-icons";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { PortableText } from "next-sanity";
-import { useQueryState } from "nuqs";
-import { useEffect, useState } from "react";
+import type { IBlog } from "@/constants/blog.constants";
+import { urlFor } from "@/lib/utils";
+import { Cross2Icon, EyeOpenIcon } from "@radix-ui/react-icons";
+import { PortableText, defineQuery } from "next-sanity";
+import Link from "next/link";
+import { sanityFetch } from "../../studio/live";
+import PortableImageComponent from "./PortableImageComponent";
 import DialogBadge from "./SmallBadge";
 import { Button } from "./ui/button";
 
+const BLOG_QUERY = defineQuery(`
+  *[_type == "blog" && slug.current == $slug][0]{
+	content,
+  }
+`);
+
 interface Props {
-	blog: IBlogWithContent;
+	blog: IBlog;
 }
 
-const MobileArticle = ({ blog }: Props) => {
-	console.log("🚀 ~ MobileArticle ~ blog:", blog);
-	const [articleId, setArticleId] = useQueryState("article");
-	const [loading, setLoading] = useState(true);
+const MobileArticle = async ({ blog }: Props) => {
+	const {
+		data: { content },
+	} = await sanityFetch({
+		query: BLOG_QUERY,
+		params: { slug: blog.slug.current },
+	});
 
-	useEffect(() => {
-		const timer = setTimeout(() => setLoading(false), 500);
-		return () => clearTimeout(timer);
-	}, []);
-
-	const urlFor = (source: SanityImageSource) =>
-		imageUrlBuilder({ dataset: "production", projectId: "4elu8cpw" }).image(
-			source,
-		);
 	const eventImageUrl = blog ? urlFor(blog.bgImage).url() : null;
-	console.log(eventImageUrl);
+	const components = {
+		types: {
+			image: PortableImageComponent,
+		},
+	};
 
 	return (
-		<div className="">
-			{loading ? (
+		<div>
+			{/* {loading ? (
 				<div className="flex items-center justify-center h-screen">
 					<ReloadIcon className="w-6 h-6 animate-spin" />
 				</div>
-			) : (
-				<>
-					<div className="pt-20">
-						<article className="relative h-48 max-w-full overflow-hidden ">
-							<div
-								className="absolute  inset-0 h-full brightness-50 -z-10"
-								style={{ backgroundImage: `url(${eventImageUrl})` }}
-							/>
-							<div className="flex flex-col items-center justify-center h-full">
-								<h1 className="flex flex-col items-center text-xl font-bold text-center text-white ">
-									{blog.title}
-								</h1>
-							</div>
-							<div className="absolute flex items-center text-xs text-white bottom-4 left-4">
-								<EyeOpenIcon className="mr-1" />
-								<p> {blog.timeToRead}Mins</p>
-							</div>
-							<div className="absolute text-white bottom-4 right-4">
-								<DialogBadge blogs={blog} />
-							</div>
+			) : ( */}
+			<>
+				<div className="pt-20">
+					<article className="relative h-48 max-w-full overflow-hidden ">
+						<div
+							className="absolute  inset-0 h-full brightness-50 -z-10"
+							style={{ backgroundImage: `url(${eventImageUrl})` }}
+						/>
+						<div className="flex flex-col items-center justify-center h-full">
+							<h1 className="flex flex-col items-center text-xl font-bold text-center text-white ">
+								{blog.title}
+							</h1>
+						</div>
+						<div className="absolute flex items-center text-xs text-white bottom-4 left-4">
+							<EyeOpenIcon className="mr-1" />
+							<p> {blog.timeToRead}Mins</p>
+						</div>
+						<div className="absolute text-white bottom-4 right-4">
+							<DialogBadge blogs={blog} />
+						</div>
+						<Link href="/">
 							<Button
-								className="absolute z-50 right-2 top-4"
+								className="absolute z-50 right-2 top-4 hover:bg-transparent hover:scale-125 transition-all hover:rotate-90"
 								variant="ghost"
 								size="icon"
-								onClick={() => setArticleId(null)}
 							>
 								<Cross2Icon className="w-6 h-6 text-white" />
 							</Button>
-						</article>
-					</div>
-
-					<div className="p-8 text-lg leading-10 ">
-						<PortableText value={blog.content} />
-					</div>
-				</>
-			)}
+						</Link>
+					</article>
+				</div>
+				<div className="p-8 text-lg leading-10 ">
+					<PortableText value={content} components={components} />
+				</div>
+			</>
 		</div>
 	);
 };
