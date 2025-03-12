4fff31e8005a8c22650ce9d021e981638423fd31
juheedhone <juheedhone12@gmail.com>
2025-03-12 12:18:01 +0530

    change icon in modal

--- src/components/IconModal.tsx
+++ src/components/IconModal.tsx
@@ -1,10 +1,12 @@
+import { Instagram, Linkedin, Twitter, Youtube } from 'lucide-react';
+
 const IconModal = () => {
 	return (
-		<div className="flex justify-center items-center space-x-6 md:space-x-4 pt-2 ">
-			<img src="/youtube.svg" alt="logo" />
-			<img src="/link.svg" alt="logo" />
-			<img src="/Vector.svg" alt="logo" />
-			<img src="/twitter.svg" alt="logo" />
+		<div className="flex justify-center items-center space-x-4 pt-2 ">
+			<Instagram className="w-6 h-6 stroke-1.5 hover:text-blue-500 transition-all duration-300" />
+			<Twitter className="w-6 h-6 stroke-1.5 hover:text-blue-500 transition-all duration-300" />
+			<Linkedin className="w-6 h-6 stroke-1.5 hover:text-blue-500 transition-all duration-300" />
+			<Youtube className="w-6 h-6 stroke-1.5 hover:text-blue-500 transition-all duration-300" />
 		</div>
 	);
 };
